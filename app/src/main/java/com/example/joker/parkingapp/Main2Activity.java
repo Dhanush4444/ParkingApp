da26c9e32e4e4e23416ed13e3eea291b467c77d0
Dhanush4444 <dhanushdazz@gmail.com>
2019-03-31 20:35:07 +0530

    Fixed infinite bug

--- app/src/main/java/com/example/joker/parkingapp/Main2Activity.java
+++ app/src/main/java/com/example/joker/parkingapp/Main2Activity.java
@@ -1,6 +1,7 @@
 package com.example.joker.parkingapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -29,7 +33,7 @@ public class Main2Activity extends AppCompatActivity {
         mUser = mAuth.getCurrentUser();
 
         if (mUser != null) {
-            startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
         } else {
             loginB.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -37,7 +41,17 @@ public class Main2Activity extends AppCompatActivity {
                     if(TextUtils.isEmpty(emailE.getText().toString().trim()) || TextUtils.isEmpty(passE.getText().toString().trim())){
                         Toast.makeText(Main2Activity.this, "Fields empty", Toast.LENGTH_SHORT).show();
                     } else{
-
+                            mAuth.signInWithEmailAndPassword(emailE.getText().toString().trim(),passE.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(Main2Activity.this, "Loggin you in", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                    } else{
+                                        Toast.makeText(Main2Activity.this, "No such email or password", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                     }
                 }
             });
