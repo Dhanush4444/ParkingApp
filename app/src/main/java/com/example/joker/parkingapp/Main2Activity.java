24870df768766314875fdf40dbcdeaef0cca8c10
Dhanush4444 <dhanushdazz@gmail.com>
2019-03-31 20:29:04 +0530

    Added code to check if fields are empty

--- app/src/main/java/com/example/joker/parkingapp/Main2Activity.java
+++ app/src/main/java/com/example/joker/parkingapp/Main2Activity.java
@@ -1,9 +1,13 @@
 package com.example.joker.parkingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -12,7 +16,8 @@ public class Main2Activity extends AppCompatActivity {
     Button loginB;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
-    EditText emailE,passE;
+    EditText emailE, passE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,19 @@ public class Main2Activity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
 
+        if (mUser != null) {
+            startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+        } else {
+            loginB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(TextUtils.isEmpty(emailE.getText().toString().trim()) || TextUtils.isEmpty(passE.getText().toString().trim())){
+                        Toast.makeText(Main2Activity.this, "Fields empty", Toast.LENGTH_SHORT).show();
+                    } else{
 
+                    }
+                }
+            });
+        }
     }
 }
